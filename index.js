76c058823ee2358d50d9caf3ab3dc8e34cb60a10
jonathan_baraza <jbaraza46@gmail.com>
2023-06-17 12:14:48 +0300

    connection to db

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import Express from "express";
 import messagesRoutes from "./routes/message/MessageRoute.js";
 import user_router from "./routes/message/user.js";
+import { connectToDB } from "./config/db.js";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
+
 const app = Express();
 
 //middleware to parse json
 app.use(Express.json());
+
+//call the function to connect to DB
+connectToDB();
 
 //create simple route
 app.get("/", (req, res) => {
@@ -14,6 +22,9 @@ app.get("/", (req, res) => {
 app.use("/message", messagesRoutes);
 app.use("/message/user", user_router);
 
-app.listen(4000, (req, res) => {
-  console.log(`Server is running on port 4000`);
+mongoose.connection.on("open", () => {
+  console.log("Connected to mongo db");
+  app.listen(4000, (req, res) => {
+    console.log(`Server is running on port 4000`);
+  });
 });
